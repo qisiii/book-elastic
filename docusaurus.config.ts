8efcfc0b152013ddc9c51f255c7f23e676da41f1
qisiii <43609115+qisiii@users.noreply.github.com>
2025-12-07 05:55:16 +0800

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: '',
+  url: 'https://onebook.qisihub.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/elastic/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
